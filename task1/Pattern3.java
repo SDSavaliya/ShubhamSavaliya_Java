5ea7489e8351a17a688c819c197d96eaf34c98df
Shubham Savaliya <87219957+SDSavaliya@users.noreply.github.com>
2022-11-17 17:18:12 +0530

    Add files via upload

---
+++ task1/Pattern3.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter limit: ");
+		int n = sc.nextInt();
+		int a=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(a++ +" ");	
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
